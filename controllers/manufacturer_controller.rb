319e71a0c442fa95399af1ce1fbae4d12ca64c0c
Gemma <gemma.percival@hotmail.co.uk>
2018-07-30 22:06:40 +0100

    heroku_setup

--- controllers/manufacturer_controller.rb
+++ controllers/manufacturer_controller.rb
@@ -1,9 +1,8 @@
 require('sinatra')
-require('sinatra/contrib/all')
+require('sinatra/contrib/all') if development?
 require_relative('../models/shop.rb')
 require_relative('../models/manufacturer.rb')
 require_relative('../models/stock.rb')
-also_reload('../models/*')
 
 get '/manufacturers' do
   @manufacturers = Manufacturer.all
--- controllers/stock_controller.rb
+++ controllers/stock_controller.rb
@@ -1,10 +1,9 @@
 require('sinatra')
-require('sinatra/contrib/all')
+require('sinatra/contrib/all') if development?
 require('pry-byebug')
 require_relative('../models/shop.rb')
 require_relative('../models/manufacturer.rb')
 require_relative('../models/stock.rb')
-also_reload('../models/*')
 
 get '/stocks' do
   @stocks = Stock.all
