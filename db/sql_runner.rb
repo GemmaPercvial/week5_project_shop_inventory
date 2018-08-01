7388621b1205cebe210af84657143e3e3d93e671
Gemma <gemma.percival@hotmail.co.uk>
2018-08-01 22:11:04 +0100

    add heroku

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -2,7 +2,9 @@ require('pg')
 class SqlRunner
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'shop_inventory', host: 'localhost' })
+      PG.connect( {dbname: 'd6714dcan9mjuo',
+        host: 'ec2-174-129-247-1.compute-1.amazonaws.com',
+        port: 5432, user: 'kvhzmfmfqtehos', password: '716fd763fed4d7d56efdfe367b49583b6f757be3677d3a72a7564d77537afb83'})
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
