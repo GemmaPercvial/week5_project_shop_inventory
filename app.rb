16857394fc1463733a56d67bdaaadad7cfa2db62
Gemma <gemma.percival@hotmail.co.uk>
2018-08-02 13:04:58 +0100

    added heroku

--- app.rb
+++ app.rb
@@ -5,8 +5,6 @@ require_relative('controllers/shop_controller')
 require_relative('controllers/stock_controller')
 require_relative('controllers/tracker_controller')
 
-also_reload('./models/*')
-
 get '/' do
   erb(:index)
 end
