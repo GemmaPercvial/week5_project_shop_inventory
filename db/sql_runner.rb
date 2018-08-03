d6f79d08ab6512207e1a324a846f74acdf70c3ad
Gemma <gemma.percival@hotmail.co.uk>
2018-08-03 13:19:46 +0100

    Changed SQL Runner credentials to match new db

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -2,9 +2,11 @@ require('pg')
 class SqlRunner
   def self.run( sql, values = [] )
     begin
-      PG.connect( {dbname: 'd6714dcan9mjuo',
-        host: 'ec2-174-129-247-1.compute-1.amazonaws.com',
-        port: 5432, user: 'kvhzmfmfqtehos', password: '716fd763fed4d7d56efdfe367b49583b6f757be3677d3a72a7564d77537afb83'})
+      db =
+      # PG.connect({ dbname: 'shop_inventory', host: 'localhost' })
+      PG.connect( {dbname: 'dpdh4gat2qral',
+        host: 'ec2-54-235-193-34.compute-1.amazonaws.com',
+        port: 5432, user: 'nhmhwceqaujxrq', password: '02a01889f26a520b1194b193a395430c4a725f54ff287e489b57b328160f9a99'})
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
